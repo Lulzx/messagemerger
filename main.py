3faf8629565f67e2b9a8eee1703787a8c0bbcdc1
Lulzx <wiserishabh@gmail.com>
2019-04-14 10:46:33 +0530

    Using built-in ptb chat_data

--- main.py
+++ main.py
@@ -8,34 +8,31 @@ import os
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-user_data = dict()
-
 
 def start(bot, update):
     update.message.reply_text("""I am a bot to help you merge messages.""")
     update.message.reply_text("""Forward a bunch of messages and send /done command when you're done, I will send you an always-at-the-bottom message with all contents concatenated!""")
 
 
-def forward(bot, update):
-    global user_data
+def forward(bot, update, chat_data):
     user_id = update.message.from_user.id
-    if user_id not in user_data:
-        user_data[user_id] = dict()
-    message = str(user_data[user_id]) + update.message.text
-    temp = {user_id: "{}\n".format(message)}
-    user_data.update(temp)
+    try:
+        messages = str(chat_data[user_id])
+
+    except KeyError:
+        messages = ""
+    chat_data[user_id] = "{}\n".format(messages + update.message.text)
 
 
-def delete(user_id):
-    global user_data
-    del user_data[user_id]
-
-
-def done(bot, update):
+def done(bot, update, chat_data):
     user_id = update.message.from_user.id
-    text = user_data[user_id]
-    update.message.reply_text(text[2:])
-    delete(user_id)
+    try:
+        messages = chat_data[user_id]
+        update.message.reply_text(messages)
+
+    except KeyError:
+        update.message.reply_text('Forward some messages.')
+    chat_data.clear()
 
 
 def error(bot, update, error):
@@ -46,8 +43,9 @@ def main():
     updater = Updater(os.environ.get('TOKEN'))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("done", done))
-    dp.add_handler(MessageHandler(Filters.forwarded, forward))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("done", done, pass_chat_data=True))
+    dp.add_handler(MessageHandler(Filters.forwarded, forward, pass_chat_data=True))
     dp.add_error_handler(error)
     updater.start_polling()
     logger.info("Ready to rock..!")
