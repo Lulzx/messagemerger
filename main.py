95b26c787dc6c9b62b712f398ac6f90c86bfc264
Lulzx <wiserishabh@gmail.com>
2019-04-21 12:17:48 +0530

    Removing unecessary stuff, code refactoring.

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import logging
 import os
 import time
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def start(bot, update):
-    update.message.reply_text("I am a bot to help you merge messages. ")
-    update.message.reply_text("Forward a bunch of messages and send /done command when you're done.")
+    update.message.reply_text("I am a bot to help you merge messages.\n"
+                              "Forward a bunch of messages and send /done command when you're done.")
 
 
 def forward(bot, update, chat_data):
@@ -32,21 +32,8 @@ def done(bot, update, chat_data):
         if len(text) <= 4096:
             update.message.reply_text(text)
         else:
-            parts = []
-            while len(text) > 0:
-                if len(text) > 4096:
-                    part = text[:4096]
-                    first_lnbr = part.rfind("\n")
-                    if first_lnbr != -1:
-                        parts.append(part[:first_lnbr])
-                        text = text[(first_lnbr + 1) :]
-                    else:
-                        parts.append(part)
-                        text = text[4096:]
-                else:
-                    parts.append(text)
-                    break
-            for part in parts:
+            messages = [text[i: i + 4096] for i in range(0, len(text), 4096)]
+            for part in messages:
                 update.message.reply_text(part)
                 time.sleep(1)
     except KeyError:
@@ -59,7 +46,7 @@ def error(bot, update, error):
 
 
 def main():
-    updater = Updater(os.environ.get('TOKEN'))
+    updater = Updater(os.environ.get("TOKEN"))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
@@ -71,5 +58,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
