56e7dd40b9ffade2ea78359072da5c511c349708
Lulzx <wiserishabh@gmail.com>
2019-06-18 15:00:59 +0530

    Added functionality to share the merged messages
    Merged messages can be shared in any other chat group or channel through inline mode.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
 import logging
 import os
+import sys
 import time
+import json
+from uuid import uuid4
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, ParseMode, \
+    InputTextMessageContent
+from telegram.ext import (Updater, CommandHandler, InlineQueryHandler, MessageHandler, Filters)
+from tinydb import TinyDB, Query
+
+try:
+    db = TinyDB('db.json')
+except:
+    db = TinyDB('C:/Users/Lulzx/My Documents/msg/db.json')
+user = Query()
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,12 +38,38 @@ def forward(bot, update, chat_data):
     chat_data[user_id] = "{}\n".format(messages + update.message.text)
 
 
+def inline(bot, update, switch_pm=None):
+    query = update.inline_query.query
+    if not switch_pm:
+        switch_pm = ['Switch to PM', 'help']
+    try:
+        search = db.get(user['message_id'] == f'{query}')
+        json_str = json.dumps(search)
+        resp = json.loads(json_str)
+        text = resp['text']
+        if search is None:
+            text = "Go to private"
+        result = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="Your message",
+                description="Click here to send in this chat",
+                input_message_content=InputTextMessageContent(
+                    "{}".format(text)))]
+        update.inline_query.answer(result, switch_pm_text=f'{switch_pm[0]}', switch_pm_parameter=f'{switch_pm[1]}')
+    except IndexError:
+        pass
+
+
 def done(bot, update, chat_data):
     user_id = update.message.from_user.id
     try:
         text = str(chat_data[user_id])
+        message_id = uuid4()
+        db.insert({'message_id': f'{message_id}', 'text': f'{text}'})
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton("share", switch_inline_query=f'{message_id}')]])
         if len(text) <= 4096:
-            update.message.reply_text(text)
+            update.message.reply_text(text, reply_markup=markup)
         else:
             messages = [text[i: i + 4096] for i in range(0, len(text), 4096)]
             for part in messages:
@@ -46,10 +85,15 @@ def error(bot, update, error):
 
 
 def main():
-    updater = Updater(os.environ.get("TOKEN"))
+    try:
+        token = sys.argv[1]
+    except:
+        token = os.environ.get("TOKEN")
+    updater = Updater(token)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(InlineQueryHandler(inline))
     dp.add_handler(CommandHandler("done", done, pass_chat_data=True))
     dp.add_handler(MessageHandler(Filters.forwarded, forward, pass_chat_data=True))
     dp.add_error_handler(error)
