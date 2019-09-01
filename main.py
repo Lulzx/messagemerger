7caf4697fc0f45bc031a3853ae4cc10b296272f5
Lulzx <wiserishabh@gmail.com>
2019-09-01 11:38:42 +0530

    changes in code for better readability

--- main.py
+++ main.py
@@ -3,20 +3,23 @@
 
 import json
 import os
+import re
 import sys
 import time
 import urllib.parse
+from functools import wraps
 from uuid import uuid4
 
 import telegram
 from logzero import logger
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent, \
-    ParseMode
-from telegram.ext import (Updater, CommandHandler, InlineQueryHandler, MessageHandler, Filters, CallbackQueryHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler)
 from tinydb import TinyDB, Query
 
 db = TinyDB('db.json')
-user = Query()
+user_db = Query()
+
+LIST_OF_ADMINS = [691609650]
 
 
 def start(update, context):
@@ -25,34 +28,45 @@ def start(update, context):
     update.message.reply_text(text)
 
 
+def send_help(update, context):
+    update.message.reply_text("Use /start to get information on how to use me.")
+
+
 def get_admin_ids(context, chat_id):
     return [admin.user.id for admin in context.bot.get_chat_administrators(chat_id)]
 
 
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(update, context, *args, **kwargs)
+
+    return wrapped
+
+
 def store_forwarded_message(update, context):
-    user_id = update.message.chat_id
+    user_id = update.message.from_user.id
     try:
         first_name = update.message.forward_from.first_name + ': '
     except AttributeError:
         first_name = "HiddenUser: "
     text = first_name + update.message.text_markdown
     scheme = [text]
-    try:
-        context.user_data[user_id].extend(scheme)
-    except KeyError:
-        context.user_data[user_id] = []
-        context.user_data[user_id].extend(scheme)
+    context.user_data.setdefault(user_id, []).extend(scheme)
 
 
-def split(update, context):
-    user_id = update.message.chat_id
+def split_messages(update, context):
+    user_id = update.message.from_user.id
     try:
-        text = ""
-        data = context.user_data[user_id]
-        for i in data:
-            text += i + "\n"
-        text = text.replace('{}: '.format(text.split(': ')[0]), '')
-        text = text.replace('\n\n', '\n').replace('\n\n', '\n').replace(u"\u2800", '')
+        current_contents = context.user_data[user_id]
+        text = "\n".join(current_contents)
+        first_name = text.split(': ')[0]
+        text = text.replace(str(first_name) + ': ', '')
+        text = re.sub(r'\n+', '\n', text).strip()
         messages = text.splitlines()
         filtered_chars = ['$', '&', '+', ',', ':', ';', '=', '?', '@', '#', '|', '<', '>', '.', '^', '*', '(', ')', '%',
                           '!', '-', '_']
@@ -65,42 +79,17 @@ def split(update, context):
         pass
     except KeyError:
         update.message.reply_text("Forward a merged message.")
-    context.user_data.clear()
-
-
-def inline(update, context, switch_pm=None):
-    query = update.inline_query.query
-    text = ""
-    if not switch_pm:
-        switch_pm = ['Switch to PM', 'help']
-    try:
-        search = db.get(user['message_id'] == query)
-        json_str = json.dumps(search)
-        resp = json.loads(json_str)
-        try:
-            text = resp['text']
-        except TypeError:
-            if search is None:
-                text = "Go to private"
-        result = [
-            InlineQueryResultArticle(
-                id=uuid4(),
-                title="Your message",
-                description="Click here to send in this chat",
-                input_message_content=InputTextMessageContent(
-                    "{}".format(text)))]
-        update.inline_query.answer(result, switch_pm_text=f'{switch_pm[0]}', switch_pm_parameter=f'{switch_pm[1]}')
-    except IndexError:
-        pass
+    finally:
+        context.user_data.clear()
 
 
 def done(update, context):
-    user_id = update.message.chat_id
+    user_id = update.message.from_user.id
     try:
         text = ""
         data = context.user_data[user_id]
         message_id = uuid4()
-        db.insert({'message_id': f'{message_id}', 'text': data})
+        db.insert({'message_id': str(message_id), 'text': data})
         if len(data) <= 4096:
             for i in data:
                 i = i.split(': ', 1)[1]
@@ -119,7 +108,8 @@ def done(update, context):
                 time.sleep(1)
     except KeyError:
         update.message.reply_text("Forward some messages.")
-    context.user_data.clear()
+    finally:
+        context.user_data.clear()
 
 
 def post(update, context):
@@ -128,19 +118,17 @@ def post(update, context):
     query = update.callback_query
     query_data = query.data.split(';')
     message_id = query_data[0]
-    search = db.get(user['message_id'] == message_id)
+    search = db.get(user_db['message_id'] == message_id)
     json_str = json.dumps(search)
     resp = json.loads(json_str)
+    data = resp.get('text')
     text = ""
-    data = resp['text']
     for i in data:
         i = i.split(': ', 1)[1]
         text += i + "\n"
     try:
         if query_data[1] == "show_dialogs":
-            text = ""
-            for i in data:
-                text += i + "\n"
+            text = "\n".join(data)
             url_msg = text.replace('_', '__').replace('*', '**')
             share_url = 'tg://msg_url?url=' + urllib.parse.quote(url_msg)
             markup = InlineKeyboardMarkup([[InlineKeyboardButton("📬 Share", url=share_url)], [
@@ -155,7 +143,7 @@ def post(update, context):
                 InlineKeyboardButton("🗣 Show names", callback_data='{};show_dialogs'.format(message_id))]])
             query.edit_message_text(text=text, reply_markup=markup, parse_mode=ParseMode.MARKDOWN)
         else:
-            search = db.search(user['user_id'] == f'{user_id}')
+            search = db.search(user_db['user_id'] == f'{user_id}')
             json_str = json.dumps(search[0])
             resp = json.loads(json_str)
             channel_id = resp['channel_id']
@@ -172,7 +160,7 @@ def post(update, context):
 
 
 def add(update, context):
-    user_id = update.message.chat_id
+    user_id = update.message.from_user.id
     channel_id = ' '.join(context.args)
     if context.bot.id in get_admin_ids(context.bot, channel_id):
         db.insert({'user_id': f'{user_id}', 'channel_id': f'{channel_id}'})
@@ -183,24 +171,18 @@ def add(update, context):
                                  text="Please double-check if the bot is an administrator in your channel.")
 
 
+@restricted
 def backup(update, context):
-    username = update.message.from_user.username
-    chat_id = update.message.chat_id
-    if username == 'Lulzx':
-        context.bot.send_document(chat_id=chat_id, document=open('db.json', 'rb'))
-    else:
-        context.bot.send_message(chat_id=chat_id, text="Only for admins for maintenance purpose.")
+    update.message.reply(document=open('db.json', 'rb'))
 
 
+@restricted
 def backup_handler(update, context):
-    file = context.bot.getFile(update.message.document.file_id)
+    file = context.bot.get_file(update.message.document.file_id)
     file_name = update.message.document.file_name
-    username = update.message.from_user.username
-    chat_id = update.message.chat_id
-    if username == 'Lulzx':
-        os.remove(file_name)
-        file.download(file_name)
-        context.bot.send_message(chat_id=chat_id, text="Alright! I have uploaded the backup.")
+    os.remove(file_name)
+    file.download(file_name)
+    update.message.reply(text="Alright! I have uploaded the backup.")
 
 
 def error_callback(update, context):
@@ -215,12 +197,11 @@ def main():
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(InlineQueryHandler(inline))
+    dp.add_handler(CommandHandler("help", send_help))
     dp.add_handler(CommandHandler("add", add))
     dp.add_handler((CallbackQueryHandler(post)))
     dp.add_handler(CommandHandler("done", done))
-    dp.add_handler(CommandHandler("split", split))
+    dp.add_handler(CommandHandler("split", split_messages))
     dp.add_handler(MessageHandler(Filters.forwarded & Filters.text, store_forwarded_message))
     dp.add_handler(CommandHandler("backup", backup))
     dp.add_handler(MessageHandler(Filters.document, backup_handler))
